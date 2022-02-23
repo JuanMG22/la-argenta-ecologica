608b3da9dc4f03c05fa14979e3c81f5ec40ab699
santio89 <santiolais1989@gmail.com>
2022-02-23 19:17:31 -0300

    products- more layout

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,25 @@
 /* PRODUCTS SECTION */
+const removeModal = (e)=>{
+    let buttonOpen = document.querySelector("#products__button--open");
+/*     let buttonClose = document.querySelector("#products__button--close"); */
+    let productContainer = document.querySelector(".products__productsContainer");
+
+    console.log(e.target)
+    if (e.target != productContainer && e.target != buttonOpen){
+        if (!productContainer.contains(e.target)){
+            productContainer.classList.remove("translateX-0");
+            document.removeEventListener("click", removeModal);
+        }
+    }
+};
+
 function toggleMore(){
     let button = document.querySelector("#products__button--open");
     let productContainer = document.querySelector(".products__productsContainer");
     
     button.addEventListener("click", ()=>{
-        productContainer.classList.add("translateX-0")
+        productContainer.classList.add("translateX-0");
+        document.addEventListener("click", removeModal)
     })
 }
 
@@ -14,6 +29,7 @@ function toggleLess(){
     
     button.addEventListener("click", ()=>{
         productContainer.classList.remove("translateX-0")
+        document.removeEventListener("click", removeModal)
     })
 }
 
